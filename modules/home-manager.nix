a26c013925d81570b74e4225cc3c9c6ab7b74e68
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-11-15 13:35:05 +0100

    Remove `apply` from `options.doomPrivateDir` in `hmModule`

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -20,7 +20,6 @@ in
         The specified directory should  contain yoour `init.el`, `config.el` and
         `packages.el` files.
       '';
-      apply = path: builtins.path { inherit path; };
     };
     extraConfig = mkOption {
       description = ''
